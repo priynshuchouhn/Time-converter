45214c4ba26a68d0ff523ba20645fa3472e502cc
Priyanshu Chouhan <priyanschouhan123@gmail.com>
2021-12-11 19:36:40 +0530

    Create program.java

---
+++ program.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Program {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int days = scanner.nextInt();
+		
+		int seconds = days*24*60*60;
+
+System.out.println(seconds);
+		
+ }
+}
